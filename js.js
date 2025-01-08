34c8f1e37643d80f0273fe6aa24383803f8c397e
Ryan Celio <ryan.celio.s.c@gmail.com>
2025-01-08 09:16:15 -0300

    Added fallback video source

--- js.js
+++ js.js
@@ -11,7 +11,7 @@ playButton.onclick = () => {
 
     paused = !paused;
 };
-
+// Pause video Olverlay
 window.addEventListener("mouseout", function (event) {
     if (!event.relatedTarget || event.relatedTarget.nodeName === "HTML") {
         const overlay = document.getElementById("overlay");
@@ -20,10 +20,24 @@ window.addEventListener("mouseout", function (event) {
     }
 });
 
+// Mouse out of page popup
 const overlay = document.getElementById("overlay");
 // Close the overlay when the button is clicked
 document.getElementById("closeOverlay").addEventListener("click", function () {
     navigator.clipboard.writeText("TIME2EARN");
     overlay.style.display = "none"; // Hide the overlay
     overlay.style.visibility = "collapse";
+});
+
+const primarySource = document.getElementById("firstVideoSource");
+
+// Add an error handler to detect when the first source fails
+primarySource.addEventListener("error", () => {
+    console.log("Primary source failed, switching to fallback...");
+    const fallbackSource = document.getElementById("secondVideoSource");
+    if (fallbackSource) {
+        video.src = fallbackSource.src; // Set fallback source
+        video.load(); // Reload the video
+        video.play(); // Play the video (optional)
+    }
 });
